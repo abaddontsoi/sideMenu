dbc71a0730576bcf9ee91d2b41a5dd4f24eef541
mrabaddon <mrabaddontsoi@gmail.com>
2023-07-25 18:41:04 +0800

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,55 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+// exports all values that are going to be used in the template
+export class HomePage {
+  // text: string = 'hasdfasdf';
+
+  users = [
+    {
+      userName: 'amouasd',
+      userEmail: 'amo@mail.com'
+    }
+  ]
+
+  username: string = '';
+  email: string = '';
+  manualModified: boolean = false;
+  emailOk:boolean = false;
+  onChange(){
+    this.email = this.username!='' && !this.manualModified ? this.username+'@mail.com':this.email;
+  }
+  
+  checkEmailFormat(email: string){
+    return email.endsWith('@mail.com')
+  }
+  onChangeEmail(){
+    this.manualModified = true;
+    // console.log(this.checkEmailFormat(this.email));
+    // console.log(this.manualModified);
+    // console.log(!this.checkEmailFormat(this.email) && this.manualModified);
+    this.emailOk = (!this.checkEmailFormat(this.email) && this.manualModified);
+  }
+
+  findUser(){
+    return this.users.find(user => user.userName == this.username && user.userEmail == this.email)
+  }
+
+  checkData(){
+    const re = '[0-9]? ';
+    return this.checkEmailFormat(this.email) && !this.username.match(re)
+  }
+
+  regUser(){
+    if (!this.findUser() && this.checkData()) {
+      this.users.push({
+        userName: this.username,
+        userEmail: this.email
+      })      
+    }
+    console.log(this.users) 
+  }
   constructor() {}
 
 }
